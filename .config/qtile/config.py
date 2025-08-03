a114b57c5b9b1bf0c9941e3fce3a2cfbb4153ab3
Guilherme Matheus <guilhermatheusdp@gmail.com>
2025-08-03 16:12:00 -0300

    Qtile + Alacritty config, old configs removal

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -15,38 +15,33 @@ keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
-    Key([mod], l, lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], r, lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], u, lazy.layout.down(), desc="Move focus down"),
-    Key([mod], d, lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], l, lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], r, lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], u, lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], d, lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], l, lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], r, lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], u, lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], d, lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on the focused window"),
-    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
-    Key([mod], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod], "l", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "Return", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], l, lazy.layout.left()),
+    Key([mod], r, lazy.layout.right()),
+    Key([mod], u, lazy.layout.down()),
+    Key([mod], d, lazy.layout.up()),
+    # Key([mod], "space", lazy.layout.next()),
+
+    Key([mod, "shift"], l, lazy.layout.shuffle_left()),
+    Key([mod, "shift"], r, lazy.layout.shuffle_right()),
+    Key([mod, "shift"], u, lazy.layout.shuffle_down()),
+    Key([mod, "shift"], d, lazy.layout.shuffle_up()),
+
+    Key([mod, "control"], l, lazy.layout.grow_left()),
+    Key([mod, "control"], r, lazy.layout.grow_right()),
+    Key([mod, "control"], u, lazy.layout.grow_down()),
+    Key([mod, "control"], d, lazy.layout.grow_up()),
+    Key([mod], "n", lazy.layout.normalize()),
+
+    # Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
+    Key([mod], "Return", lazy.spawn(terminal)),
+
+    # Key([mod], "Tab", lazy.next_layout()),
+    Key([mod], "q", lazy.window.kill()),
+    Key([mod], "f", lazy.window.toggle_fullscreen()),
+    Key([mod], "space", lazy.window.toggle_floating()),
+    Key([mod], "r", lazy.reload_config()),
+    Key([mod], "l", lazy.shutdown()),
+    Key([mod], "Return", lazy.spawncmd()),
 ]
 
 # Add key bindings to switch VTs in Wayland.
@@ -68,18 +63,15 @@ groups = [Group(i) for i in "1234"]
 for i in groups:
     keys.extend(
         [
-            Key([mod], i.name, lazy.group[i.name].toscreen(),
-                desc=f"Switch to group {i.name}"),
-
-            Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-                desc=f"Switch to & move focused window to group {i.name}")
+            Key([mod], i.name, lazy.group[i.name].toscreen()),
+            Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True))
         ]
     )
 
 layouts = [
     layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
-    layout.Max(),
     # Try more layouts by unleashing below layouts.
+    # layout.Max(),
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
@@ -103,32 +95,18 @@ screens = [
     Screen(
         bottom=bar.Bar(
             [
-                # widget.CurrentLayout(),
-                widget.GroupBox(),
+                widget.GroupBox(highlight_method="text", this_current_screen_border="00ff00"),
                 widget.Prompt(),
                 widget.WindowName(),
-                widget.ThermalSensor(),
-                widget.Memory(measure_mem="G"),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
-                widget.Systray(),
-                widget.Clock(format="%H:%M:%S %d/%m/%Y"),
-                # widget.QuickExit(),
+                widget.Systray(), # NB Systray is incompatible with Wayland, consider using widget.StatusNotifier() instead
+                widget.ThermalSensor(update_interval=3.0),
+                widget.Memory(measure_mem="G", format="{MemUsed:.1f}G/{MemTotal:.1f}G", update_interval=3.0),
+                widget.Clock(format="%d/%m/%Y, %H:%M", update_interval=60.0),
             ],
             20,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+            border_width=[1, 0, 0, 0],
+            border_color=["ffffff", "000000", "000000", "000000"]
         ),
-        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
-        # By default we handle these events delayed to already improve performance, however your system might still be struggling
-        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
-        # x11_drag_polling_rate = 60,
     ),
 ]
 
@@ -140,7 +118,7 @@ mouse = [
 ]
 
 dgroups_key_binder = None
-dgroups_app_rules = []  # type: list
+dgroups_app_rules = []
 follow_mouse_focus = True
 bring_front_click = False
 floats_kept_above = True

384aa33c402967fd74bb23dc59aad36590dd8cae
Guilhermatheus <guilhermatheusdp@gmail.com>
2025-09-15 18:49:21 -0300

    Corrections and cleaning

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -15,14 +15,10 @@ r = "right"
 
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
     Key([mod], l, lazy.layout.left()),
     Key([mod], r, lazy.layout.right()),
     Key([mod], u, lazy.layout.down()),
     Key([mod], d, lazy.layout.up()),
-    # Key([mod], "space", lazy.layout.next()),
 
     Key([mod, "shift"], l, lazy.layout.shuffle_left()),
     Key([mod, "shift"], r, lazy.layout.shuffle_right()),
@@ -112,7 +108,7 @@ screens = [
     ),
 ]
 
-# Drag floating layouts.
+
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
@@ -142,8 +138,6 @@ auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
 
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = False
 
 # When using the Wayland backend, this can be used to configure input devices.
@@ -153,12 +147,5 @@ wl_input_rules = None
 wl_xcursor_theme = None
 wl_xcursor_size = 24
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
+
 wmname = "LG3D"

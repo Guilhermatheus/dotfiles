24bb7fd3a20e48f4cb9d0d94cea1b7b146826f29
Guilhermatheus <guilhermatheusdp@gmail.com>
2025-09-19 11:17:14 -0300

    Syntax and indentation corrections

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,7 +1,7 @@
 from libqtile import bar, layout, qtile, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.log_utils import logger
+#from libqtile.log_utils import logger # For debugging
 import os, random
 
 
@@ -9,141 +9,148 @@ mod = 'mod4'
 terminal = os.getenv('TERMINAL', '')
 browser = os.getenv('BROWSER', '')
 
-
-u = 'up'
-d = 'down'
-l = 'left'
-r = 'right'
+u, d, l, r = 'up', 'down', 'left', 'right'
 
 # Switch between first and second group
 @lazy.function
 def switch(qtile) -> None:
-    if qtile.current_group == qtile.groups[0]:
-        qtile.groups[1].toscreen()
-    else: qtile.groups[0].toscreen()
+		if qtile.current_group == qtile.groups[0]:
+				qtile.groups[1].toscreen()
+		else: qtile.groups[0].toscreen()
 
 
 keys = [
-    Key([mod], 'tab', switch()),
+	Key([mod], 'tab', switch()),
 
-    Key([mod], l, lazy.layout.left()),
-    Key([mod], r, lazy.layout.right()),
-    Key([mod], u, lazy.layout.down()),
-    Key([mod], d, lazy.layout.up()),
+	Key([mod], l, lazy.layout.left()),
+	Key([mod], r, lazy.layout.right()),
+	Key([mod], u, lazy.layout.down()),
+	Key([mod], d, lazy.layout.up()),
 
-    Key([mod, 'shift'], l, lazy.layout.shuffle_left()),
-    Key([mod, 'shift'], r, lazy.layout.shuffle_right()),
-    Key([mod, 'shift'], u, lazy.layout.shuffle_down()),
-    Key([mod, 'shift'], d, lazy.layout.shuffle_up()),
+	Key([mod, 'shift'], l, lazy.layout.shuffle_left()),
+	Key([mod, 'shift'], r, lazy.layout.shuffle_right()),
+	Key([mod, 'shift'], u, lazy.layout.shuffle_down()),
+	Key([mod, 'shift'], d, lazy.layout.shuffle_up()),
 
-    Key([mod, 'control'], l, lazy.layout.grow_left()),
-    Key([mod, 'control'], r, lazy.layout.grow_right()),
-    Key([mod, 'control'], u, lazy.layout.grow_down()),
-    Key([mod, 'control'], d, lazy.layout.grow_up()),
-    # Key([mod], 'n', lazy.layout.normalize()),
+	Key([mod, 'control'], l, lazy.layout.grow_left()),
+	Key([mod, 'control'], r, lazy.layout.grow_right()),
+	Key([mod, 'control'], u, lazy.layout.grow_down()),
+	Key([mod, 'control'], d, lazy.layout.grow_up()),
+	# Key([mod], 'n', lazy.layout.normalize()),
 
-    # Key([mod, 'shift'], 'Return', lazy.layout.toggle_split()),
-    # Key([mod], 'Return', lazy.spawn(terminal)),
+	# Key([mod, 'shift'], 'Return', lazy.layout.toggle_split()),
+	# Key([mod], 'Return', lazy.spawn(terminal)),
 
-    # Key([mod], 'Tab', lazy.next_layout()),
-    Key([mod], 'q', lazy.window.kill()),
-    Key([mod], 'f', lazy.window.toggle_fullscreen()),
-    Key([mod], 'space', lazy.window.toggle_floating()),
-    Key([mod], 'r', lazy.reload_config()),
-    Key([mod], 'l', lazy.shutdown()),
-    
-    Key([mod], 'Return', lazy.spawn('rofi -show drun')),
-    Key([mod], 'period', lazy.spawn(os.path.expanduser('~/.config/bin/dmoji.sh'))),
-    
-    Key([mod], 'z', lazy.spawn('pcmanfm')),
-    Key([mod], 'x', lazy.spawn(browser)),
-    Key([mod], 'c', lazy.spawn(terminal))
+	Key([mod], 'q', lazy.window.kill()),
+	Key([mod], 'f', lazy.window.toggle_fullscreen()),
+	Key([mod], 'space', lazy.window.toggle_floating()),
+	Key([mod], 'r', lazy.reload_config()),
+	Key([mod], 'l', lazy.shutdown()),
+
+	Key([mod], 'Return', lazy.spawn('rofi -show drun')),
+	Key([mod], 'period', lazy.spawn(os.path.expanduser('~/.config/bin/dmoji.sh'))),
+	
+	Key([mod], 'z', lazy.spawn('pcmanfm')),
+	Key([mod], 'x', lazy.spawn(browser)),
+	Key([mod], 'c', lazy.spawn(terminal))
 ]
 
 # Add key bindings to switch VTs in Wayland.
 # We can't check qtile.core.name in default config as it is loaded before qtile is started
 # We therefore defer the check until the key binding is run by using .when(func=...)
-for vt in range(1, 8):
-    keys.append(
-        Key(
-            ['control', 'mod1'],
-            f'f{vt}',
-            lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == 'wayland'),
-            desc=f'Switch to VT{vt}',
-        )
-    )
+#for vt in range(1, 8):
+#	keys.append(
+#		Key(
+#			['control', 'mod1'],
+#			f'f{vt}',
+#			lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == 'wayland'),
+#			desc=f'Switch to VT{vt}',
+#		)
+#	)
 
 
-groups = [Group(i) for i in '1234']
+groups = [:9]
+group_labels = ["1", "2", "•", "•"]
+
+
+for i in range(1, group_labels.len()):
+	groups.append(
+		Group(
+			name = str(i)
+			label = group_labels[i]
+		)
+	)
+
 
 for i in groups:
-    keys.extend(
-        [
-            Key([mod], i.name, lazy.group[i.name].toscreen()),
-            Key([mod, 'shift'], i.name, lazy.window.togroup(i.name, switch_group=True))
-        ]
-    )
+	keys.extend(
+		[
+			Key([mod], i.name, lazy.group[i.name].toscreen()),
+			Key([mod, 'shift'], i.name, lazy.window.togroup(i.name, switch_group=True))
+		]
+	)
+
 
 layouts = [
-    layout.MonadTall(
-        margin=16,
-        border_width=2,
-        border_focus='#00c000',
-        border_normal='#ffffff',
-        border_on_single=True
-        )
+	layout.MonadTall(
+		margin=16,
+		border_width=2,
+		border_focus='#00c000',
+		border_normal='#ffffff',
+		border_on_single=True
+	)
 ]
 
 widget_defaults = dict(
-    font='terminus',
-    foreground='ff7f27',
-    fontsize=12,
-    padding=3,
+	font='terminus',
+	foreground='ff7f27',
+	fontsize=12,
+	padding=3,
 )
 extension_defaults = widget_defaults.copy()
 
 backgrounds_path = os.path.expanduser('~/.config/backgrounds/')
 
 screens = [
-    Screen(
-        wallpaper=backgrounds_path + random.choice(os.listdir(backgrounds_path)),
-        wallpaper_mode='fill',
-        bottom=bar.Bar(
-            [
-                widget.GroupBox(highlight_method='text', this_current_screen_border=random.choice(['#00c000', '#ffff00', '#ff0000', '#003cff', '#42fcff', '#d535d9'])),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Notify(),
-                widget.Systray(), # NB Systray is incompatible with Wayland, consider using widget.StatusNotifier() instead
-                widget.ThermalSensor(update_interval=1.0),
-                widget.Clock(format='%d/%m/%Y, %H:%M', update_interval=60.0),
-            ],
-            20,
-            border_width=[2, 0, 0, 0],
-            border_color=['ff7f27', '000000', '000000', '000000']
-        ),
-    ),
+	Screen(
+		wallpaper=backgrounds_path + random.choice(os.listdir(backgrounds_path)),
+		wallpaper_mode='fill',
+		bottom=bar.Bar(
+			[
+				widget.GroupBox(highlight_method='text', this_current_screen_border=random.choice(['#00c000', '#ffff00', '#ff0000', '#003cff', '#42fcff', '#d535d9'])),
+				widget.Prompt(),
+				widget.WindowName(),
+				widget.Notify(),
+				widget.Systray(), # NB Systray is incompatible with Wayland, consider using widget.StatusNotifier() instead
+				widget.ThermalSensor(update_interval=1.0),
+				widget.Clock(format='%d/%m/%Y, %H:%M', update_interval=60.0),
+			],
+			20,
+			border_width=[2, 0, 0, 0],
+			border_color=['ff7f27', '000000', '000000', '000000']
+		),
+	),
 ]
 
 
 mouse = [
-    Drag([mod], 'Button1', lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], 'Button3', lazy.window.set_size_floating(), start=lazy.window.get_size()),
-    Click([mod], 'Button2', lazy.window.bring_to_front()),
+	Drag([mod], 'Button1', lazy.window.set_position_floating(), start=lazy.window.get_position()),
+	Drag([mod], 'Button3', lazy.window.set_size_floating(), start=lazy.window.get_size()),
+	Click([mod], 'Button2', lazy.window.bring_to_front()),
 ]
 
 
 floating_layout = layout.Floating(
-    float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
-        *layout.Floating.default_float_rules,
-        Match(wm_class='confirmreset'),  # gitk
-        Match(wm_class='makebranch'),  # gitk
-        Match(wm_class='maketag'),  # gitk
-        Match(wm_class='ssh-askpass'),  # ssh-askpass
-        Match(title='branchdialog'),  # gitk
-        Match(title='pinentry'),  # GPG key password entry
-    ]
+	float_rules=[
+		# Run the utility of `xprop` to see the wm class and name of an X client.
+		*layout.Floating.default_float_rules,
+		Match(wm_class='confirmreset'),	# gitk
+		Match(wm_class='makebranch'),	# gitk
+		Match(wm_class='maketag'),	# gitk
+		Match(wm_class='ssh-askpass'),	# ssh-askpass
+		Match(title='branchdialog'),	# gitk
+		Match(title='pinentry'),	# GPG key password entry
+	]
 )
 
 dgroups_key_binder = None

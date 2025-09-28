9f3e4c0eba7347c6f3cc56d7ad9aa24e1c174f4b
Guilhermatheus <guilhermatheusdp@gmail.com>
2025-09-28 12:26:16 -0300

    Keymap corrections

--- .config/nvim/lua/nvim-keymaps.lua
+++ .config/nvim/lua/nvim-keymaps.lua
@@ -3,12 +3,12 @@ vim.keymap.set("n", "Y", "y$", {desc = "Yank to end of line"})
 
 vim.keymap.set("n", "U", "<C-r>", {desc = "Undo"})
 
-vim.keymap.set("n", "<S-Up>", ":m .+1<CR>==", {desc = "Move line up"})
-vim.keymap.set("v", "<S-Up>", ":m '>+1<CR>gv=gv", {desc = "Move selection up"})
-vim.keymap.set("n", "<S-Down>", ":m .-2<CR>==", {desc = "Move line down"})
-vim.keymap.set("v", "<S-Down>", ":m '<-2<CR>gv=gv", {desc = "Move selection down"})
+vim.keymap.set("n", "<S-Down>", ":m .+1<CR>==", {desc = "Move line up"})
+vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv", {desc = "Move selection up"})
+vim.keymap.set("n", "<S-Up>", ":m .-2<CR>==", {desc = "Move line down"})
+vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv", {desc = "Move selection down"})
 
 vim.keymap.set("v", "<", "<gv", {desc = "Remove indent and reselect"})
-vim.keymap.set("v", "<", ">gv", {desc = "Add indent and reselect"})
+vim.keymap.set("v", ">", ">gv", {desc = "Add indent and reselect"})
 
 vim.keymap.set("n", "E", ":Explore<CR>", {desc = "Open file explorer"})

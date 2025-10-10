7f2302bf5d2894892ff4a0f75ab4c91342862da8
Guilhermatheus <guilhermatheusdp@gmail.com>
2025-10-10 08:35:27 -0300

    Use as actual script

--- .config/bin/music-list-download.sh
+++ .config/bin/music-list-download.sh
@@ -1,13 +1,32 @@
-alias downmus='yt-dlp -x --audio-format mp3 --audio-quality 6 --embed-metadata --convert-thumbnails jpg --embed-thumbnail'
 
-function downlist {
-	while read line; do
-		char=$(echo "$line" | cut -c 1)
-		if [ "$char" != "@" ]; then
-			downmus $line
-			if [ $? -eq 0 ]; then
-				sed -i "s,$line,@&,g" $1
-			fi
+alias download-music='yt-dlp -x --audio-format mp3 --audio-quality 6  --download-archive download-archive.txt --convert-thumbnails jpg  --embed-metadata --embed-thumbnail'
+
+read -p "File path: " path
+
+if [ -f "$path"]; then
+	
+	
+	while read link; do
+		
+		download-music "$link"
+		
+		if [ $? -eq 0 ]; then
+			grep -v "$link" $path > $path.tmp
+			mv -f $path.tmp $path
 		fi
-	done < $1
-}
+
+	done < $path
+
+	if [ ! -s $path ]; then
+		rm -f $path
+	fi
+
+	for music in *.mp3; do
+		[ -f "$music" ] || break
+		mp3gain -r "$music"
+	done
+
+
+else
+	echo 'ERROR: File not found'
+fi

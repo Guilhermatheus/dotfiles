d07bc91cdb7c633ea77c5e10af92a55807abfaf9
Guilhermatheus <guilhermatheusdp@gmail.com>
2025-10-25 13:06:08 -0300

    Start and end of line shortcut

--- .config/nvim/lua/nvim-keymaps.lua
+++ .config/nvim/lua/nvim-keymaps.lua
@@ -1,12 +1,15 @@
 
-vim.keymap.set("n", "Y", "y$", {desc = "Yank to end of line"})
-
 vim.keymap.set("n", "U", "<C-r>", {desc = "Undo"})
 
 vim.keymap.set("n", "<S-Down>", ":m .+1<CR>==", {desc = "Move line up"})
 vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv", {desc = "Move selection up"})
 vim.keymap.set("n", "<S-Up>", ":m .-2<CR>==", {desc = "Move line down"})
 vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv", {desc = "Move selection down"})
+
+vim.keymap.set("n", "<S-Right>", "$")
+vim.keymap.set("v", "<S-Right>", "$")
+vim.keymap.set("n", "<S-Left>", "0")
+vim.keymap.set("v", "<S-Left>", "0")
 
 vim.keymap.set("v", "<", "<gv", {desc = "Remove indent and reselect"})
 vim.keymap.set("v", ">", ">gv", {desc = "Add indent and reselect"})

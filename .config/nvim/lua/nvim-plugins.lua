2553b5160d4f7ac8568551264037e9def2e7b397
Guilhermatheus <guilhermatheusdp@gmail.com>
2025-10-27 18:49:07 -0300

    Moved Lazy load to init.lua

--- .config/nvim/lua/nvim-plugins.lua
+++ .config/nvim/lua/nvim-plugins.lua
@@ -1,29 +1,12 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-
-end
-vim.opt.rtp:prepend(lazypath)
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
-local plugins = {
+return {
 	{
 		"bluz71/vim-moonfly-colors",
 		name = "moonfly",
 		lazy = false,
-		priority = 1000
+		priority = 1000,
+		config = function ()
+			vim.cmd.colorscheme("moonfly")
+		end
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -39,7 +22,3 @@ local plugins = {
 		end
 	},
 }
-
-require("lazy").setup({plugins, Null})
-
-vim.cmd.colorscheme("moonfly")

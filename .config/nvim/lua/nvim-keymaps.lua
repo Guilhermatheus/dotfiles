a9ed4bd2c0b1863d0dbc380c02036636035fe545
Guilhermatheus <guilhermatheusdp@gmail.com>
2025-10-29 23:19:10 -0300

    Ctrl to move lines, improved syntax

--- .config/nvim/lua/nvim-keymaps.lua
+++ .config/nvim/lua/nvim-keymaps.lua
@@ -1,21 +1,24 @@
 
-vim.keymap.set("n", "U", "<C-r>", {desc = "Undo"})
+vim.keymap.set("n", "U", "<C-r>")
 
-vim.keymap.set("n", "<S-Down>", ":m .+1<CR>==", {desc = "Move line up"})
-vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv", {desc = "Move selection up"})
-vim.keymap.set("n", "<S-Up>", ":m .-2<CR>==", {desc = "Move line down"})
-vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv", {desc = "Move selection down"})
+vim.keymap.set("n", "<C-Down>", ":m .+1<CR>==")
+vim.keymap.set("v", "<C-Down>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("n", "<C-Up>", ":m .-2<CR>==")
+vim.keymap.set("v", "<C-Up>", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<S-Right>", "$")
-vim.keymap.set("v", "<S-Right>", "$")
-vim.keymap.set("n", "<S-Left>", "0")
-vim.keymap.set("v", "<S-Left>", "0")
+local modes = "nv"
+for c in modes:gmatch"." do
+	vim.keymap.set(c, "<S-Up>", "<C-d>")
+	vim.keymap.set(c, "<S-Down>", "<C-u>")
+	vim.keymap.set(c, "<S-Right>", "$")
+	vim.keymap.set(c, "<S-Left>", "0")
+end
 
 vim.keymap.set("n", "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
-vim.keymap.set("v", "<", "<gv", {desc = "Remove indent and reselect"})
-vim.keymap.set("v", ">", ">gv", {desc = "Add indent and reselect"})
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
 vim.keymap.set("n", "E", ":Neotree<CR>")
 vim.keymap.set("n", "<C-f>", ":%s/")

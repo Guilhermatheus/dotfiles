0ed45922f36c307540239289019dbfe797f9a0be
Guilhermatheus <guilhermatheusdp@gmail.com>
2025-10-31 13:34:41 -0300

    Fixed ctrl up down and unnecessary syntax

--- .config/nvim/lua/nvim-keymaps.lua
+++ .config/nvim/lua/nvim-keymaps.lua
@@ -1,18 +1,15 @@
 
 vim.keymap.set("n", "U", "<C-r>")
 
-vim.keymap.set("n", "<C-Down>", ":m .+1<CR>==")
-vim.keymap.set("v", "<C-Down>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("n", "<C-Up>", ":m .-2<CR>==")
 vim.keymap.set("v", "<C-Up>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "<C-Down>", ":m .+1<CR>==")
+vim.keymap.set("v", "<C-Down>", ":m '>+1<CR>gv=gv")
 
-local modes = "nv"
-for c in modes:gmatch"." do
-	vim.keymap.set(c, "<S-Up>", "<C-d>")
-	vim.keymap.set(c, "<S-Down>", "<C-u>")
-	vim.keymap.set(c, "<S-Right>", "$")
-	vim.keymap.set(c, "<S-Left>", "0")
-end
+vim.keymap.set({"n", "v"}, "<S-Up>", "<C-u>")
+vim.keymap.set({"n", "v"}, "<S-Down>", "<C-d>")
+vim.keymap.set({"n", "v"}, "<S-Right>", "$")
+vim.keymap.set({"n", "v"}, "<S-Left>", "0")
 
 vim.keymap.set("n", "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })

2b61159914d7a93efbac17e991353da2fa32ee90
Guilhermatheus <guilhermatheusdp@gmail.com>
2025-11-27 12:24:14 -0300

    Changed screenshots folder

--- bin/print-screen.sh
+++ bin/print-screen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$1" == 'whole' ]; then
-	maim -ou | tee ~/.local/share/Trash/files/$(date +%s).png | xclip -selection clipboard -t image/png
+	maim -ou | tee ~/Pictures/Screenshot/$(date +%s).png | xclip -selection clipboard -t image/png
 elif [ "$1" == 'selection' ]; then
-	maim -sou | tee ~/.local/share/Trash/files/$(date +%s).png | xclip -selection clipboard -t image/png
+	maim -sou | tee ~/Pictures/Screenshot/$(date +%s).png | xclip -selection clipboard -t image/png
 fi

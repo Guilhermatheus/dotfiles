2cd44b0cd34fa9557c7bbda89452ee6d461e7640
Guilhermatheus <guilhermatheusdp@gmail.com>
2026-01-18 12:38:50 -0300

    JSINdebted fixes

--- bin/JSINdebted.sh
+++ bin/JSINdebted.sh
@@ -5,11 +5,11 @@ if [ ! -f d.json ]; then
 fi
 
 if [ $# -eq 3 ]; then
-	jq ".\"$(echo $1 | iconv -t ASCII//TRANSLIT)\" += {\"$(echo $3 | iconv -t ASCII//TRANSLIT)\": {value: $2, date: \"$(date "+%D %T")\"}}" d.json > d.json.tmp
+	jq ".\"$1\" += {\"$3\": {value: $2, date: \"$(date "+%D %T")\"}}" d.json > d.json.tmp
 elif [ $# -eq 2 ]; then
-	jq "del(.$(echo $1 | iconv -t ASCII//TRANSLIT).$(echo $2 | iconv -t ASCII//TRANSLIT))" d.json > d.json.tmp
+	jq "del(.$1.$2)" d.json > d.json.tmp
 elif [ $# -eq 1 ]; then
-	jq "del(.$(echo $1 | iconv -t ASCII//TRANSLIT))" d.json > d.json.tmp
+	jq "del(.$1" d.json > d.json.tmp
 else
 	keys=$(jq 'keys[]' d.json -r)
 
@@ -24,6 +24,8 @@ else
 	exit 0
 fi
 
-mv -f d.json.tmp d.json
+if [ $? -eq 0 ]; then
+	mv -f d.json.tmp d.json
+fi
 
 exit 0

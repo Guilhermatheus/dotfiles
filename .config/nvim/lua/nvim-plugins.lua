224942779108260ff8b900621028acb4c7e31d49
Guilhermatheus <guilhermatheusdp@gmail.com>
2026-01-26 12:54:09 -0300

    Add: gitsigns

--- .config/nvim/lua/nvim-plugins.lua
+++ .config/nvim/lua/nvim-plugins.lua
@@ -1,17 +1,21 @@
-vim.pack.add({
+vim.pack.add{
 	{ src = "https://github.com/bluz71/vim-moonfly-colors" },
 	{ src = "https://github.com/windwp/nvim-autopairs"},
 	{ src = "https://github.com/catgoose/nvim-colorizer.lua" },
+	{ src = "https://github.com/lewis6991/gitsigns.nvim"},
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-})
+}
 
 vim.cmd.colorscheme("moonfly")
 
 require "nvim-autopairs".setup()
 require "colorizer".setup()
+require "gitsigns".setup{
+	current_line_blame = true
+}
 
-require "nvim-treesitter.configs".setup({
+require "nvim-treesitter.configs".setup{
 	ensure_installed = {"python", "html", "css", "c_sharp"},
 	highlight = {enable = true},
-	indent = {enable = true},
-})
+	indent = {enable = true}
+}

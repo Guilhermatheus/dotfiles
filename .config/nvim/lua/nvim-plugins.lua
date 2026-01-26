50dd559e7c453bbd326cebb738b3168e9af4cbf4
Guilhermatheus <guilhermatheusdp@gmail.com>
2026-01-26 14:09:20 -0300

    Add: preview hunk keymap

--- .config/nvim/lua/nvim-plugins.lua
+++ .config/nvim/lua/nvim-plugins.lua
@@ -1,8 +1,8 @@
 vim.pack.add{
 	{ src = "https://github.com/bluz71/vim-moonfly-colors" },
-	{ src = "https://github.com/windwp/nvim-autopairs"},
+	{ src = "https://github.com/windwp/nvim-autopairs" },
 	{ src = "https://github.com/catgoose/nvim-colorizer.lua" },
-	{ src = "https://github.com/lewis6991/gitsigns.nvim"},
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 }
 
@@ -11,7 +11,11 @@ vim.cmd.colorscheme("moonfly")
 require "nvim-autopairs".setup()
 require "colorizer".setup()
 require "gitsigns".setup{
-	current_line_blame = true
+	current_line_blame = true,
+	on_attach = function()
+		local gitsigns = require "gitsigns"
+		vim.keymap.set("n", "D", gitsigns.preview_hunk_inline)
+	end
 }
 
 require "nvim-treesitter.configs".setup{

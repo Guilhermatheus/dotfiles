cf7dfa5dc92308fa021fc710ecee00e120089672
Guilhermatheus <guilhermatheusdp@gmail.com>
2026-02-05 12:09:37 -0300

    Added: indent-o-matic plugin

--- .config/nvim/lua/nvim-plugins.lua
+++ .config/nvim/lua/nvim-plugins.lua
@@ -4,11 +4,13 @@ vim.pack.add{
 	{ src = "https://github.com/catgoose/nvim-colorizer.lua" },
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/Darazaki/indent-o-matic.git" },
 }
 
 vim.cmd.colorscheme("moonfly")
 
 require "nvim-autopairs".setup()
+require "indent-o-matic".setup{}
 require "colorizer".setup()
 require "gitsigns".setup{
 	current_line_blame = true,

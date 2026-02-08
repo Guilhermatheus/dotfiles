a29073318aaab3d6c4544d9f66dc6dc949694fae
Guilhermatheus <guilhermatheusdp@gmail.com>
2026-02-08 13:56:06 -0300

    Change: Fast movement style

--- .config/nvim/lua/nvim-keymaps.lua
+++ .config/nvim/lua/nvim-keymaps.lua
@@ -1,5 +1,4 @@
 vim.keymap.set("n", "U", "<C-r>") -- Undo
-vim.keymap.set({"n", "v"}, "D", '"_d') -- Delete fr
 
 -- Move selected lines
 vim.keymap.set("n", "<C-Up>", ":m .-2<CR>==")
@@ -8,10 +7,14 @@ vim.keymap.set("n", "<C-Down>", ":m .+1<CR>==")
 vim.keymap.set("v", "<C-Down>", ":m '>+1<CR>gv=gv")
 
 -- Faster movement
-vim.keymap.set({"n", "v"}, "<S-Up>", "{")
-vim.keymap.set({"n", "v"}, "<S-Down>", "}")
-vim.keymap.set("i", "<S-Up>", "<C-o>{")
-vim.keymap.set("i", "<S-Down>", "<C-o>}")
+vim.keymap.set({"n", "v"}, "<S-Up>", "5k")
+vim.keymap.set({"n", "v"}, "<S-Down>", "5j")
+vim.keymap.set({"n", "v"}, "<S-Left>", "5h")
+vim.keymap.set({"n", "v"}, "<S-Right>", "5l")
+vim.keymap.set({"i"}, "<S-Up>", "<C-o>5k")
+vim.keymap.set({"i"}, "<S-Down>", "<C-o>5j")
+vim.keymap.set({"i"}, "<S-Left>", "<C-o>5h")
+vim.keymap.set({"i"}, "<S-Right>", "<C-o>5l")
 
 -- Better movement for wrapping lines
 vim.keymap.set("n", "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -27,6 +30,6 @@ vim.keymap.set("n", "<C-r>", ":%s/<C-r><C-w>/<C-r><C-w>/g") -- Better replace
 vim.keymap.set("n", "<C-i>", "gg<CR>=G<CR>''<CR>") -- Tab Re-indent
 vim.keymap.set("n", "<C-o>", ":%s/	/    /g<CR>") -- Space Re-indent
 
--- Quicker
+-- "Aliases"
 vim.keymap.set({"n", "v"}, ";", ":")
 vim.keymap.set({"n", "v"}, "!", ":!")

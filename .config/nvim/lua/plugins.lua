1f682694345dc9d3a4e6713104445da36be2a5d8
Guilhermatheus <guilhermatheusdp@gmail.com>
2026-02-11 16:40:27 -0300

    Change: gitsigns signs, added keymaps

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -17,24 +17,28 @@ require "blink.cmp".setup{}
 require "gitsigns".setup{
 	signs = {
 		add = { text = '+' },
-		change = { text = '~' },
+		change = { text = '•' },
 		delete = { text = '_' },
 		topdelete = { text = '‾' },
-		changedelete = { text = '~' },
+		changedelete = { text = '•' },
 		untracked = { text = '?' },
 	},
 	signs_staged = {
 		add = { text = '+' },
-		change = { text = '~' },
+		change = { text = '•' },
 		delete = { text = '_' },
 		topdelete = { text = '‾' },
-		changedelete = { text = '~' },
+		changedelete = { text = '•' },
 		untracked = { text = '?' },
 	},
 	current_line_blame = true,
 	on_attach = function()
 		local gitsigns = require "gitsigns"
-		vim.keymap.set("n", "<leader>d", gitsigns.preview_hunk_inline)
+		vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk_inline, {desc = 'Preview hunk'})
+		vim.keymap.set("n", "<leader>gs", gitsigns.stage_hunk, {desc = 'Stage hunk'})
+		vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk, {desc = 'Reset hunk'})
+		vim.keymap.set("n", "<leader>gbs", gitsigns.stage_buffer, {desc = 'Stage buffer'})
+		vim.keymap.set("n", "<leader>gbr", gitsigns.reset_buffer, {desc = 'Reset buffer'})
 	end
 }
 

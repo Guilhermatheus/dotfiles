e259b9d1e5c4608f9028813cde7f90c365a9c4ca
Guilhermatheus <guilhermatheusdp@gmail.com>
2026-02-13 17:37:18 -0300

    Change: gitsigns keymaps

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -34,11 +34,11 @@ require "gitsigns".setup{
 	current_line_blame = true,
 	on_attach = function()
 		local gitsigns = require "gitsigns"
-		vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk_inline, {desc = 'Preview hunk'})
-		vim.keymap.set("n", "<leader>gs", gitsigns.stage_hunk, {desc = 'Stage hunk'})
-		vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk, {desc = 'Reset hunk'})
-		vim.keymap.set("n", "<leader>gbs", gitsigns.stage_buffer, {desc = 'Stage buffer'})
-		vim.keymap.set("n", "<leader>gbr", gitsigns.reset_buffer, {desc = 'Reset buffer'})
+		vim.keymap.set("n", "<leader>gs", gitsigns.preview_hunk_inline, {desc = '[S]ee hunk'})
+		vim.keymap.set("n", "<leader>ga", gitsigns.stage_hunk, {desc = '[A]dd hunk'})
+		vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk, {desc = '[R]estore hunk'})
+		vim.keymap.set("n", "<leader>gba", gitsigns.stage_buffer, {desc = '[A]dd buffer'})
+		vim.keymap.set("n", "<leader>gbr", gitsigns.reset_buffer, {desc = '[R]estore buffer'})
 	end
 }
 

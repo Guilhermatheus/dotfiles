35318aac7e58ba06a52d789e3e9884cb63002bcd
Guilhermatheus <guilhermatheusdp@gmail.com>
2026-02-27 15:38:04 -0300

    Add: theme.lua, wallpaper

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -32,9 +32,7 @@ do
 end
 
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-beautiful.font = 'terminus bold 8'
-beautiful.border_width = 0
+beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 
 
 for _, preset in pairs(naughty.config.presets) do
@@ -123,7 +121,7 @@ local function set_wallpaper(s)
         if type(wallpaper) == "function" then
             wallpaper = wallpaper(s)
         end
-        gears.wallpaper.maximized(wallpaper, s, true)
+        gears.wallpaper.maximized(wallpaper, s)
     end
 end
 
@@ -257,47 +255,16 @@ root.keys(gears.table.join(
 ))
 
 local clientkeys = gears.table.join(
-    awful.key({ modkey }, "f",
-    function (c)
-        c.fullscreen = not c.fullscreen
-        c:raise()
-    end,
-    {description = "toggle fullscreen", group = "client"}),
+    --awful.key({ modkey }, "f",
+    --function (c)
+    --    c.fullscreen = not c.fullscreen
+    --    c:raise()
+    --end,
+    --{description = "toggle fullscreen", group = "client"}),
     awful.key({ modkey }, "q",      function (c) c:kill()                         end,
     {description = "close", group = "client"}),
-    awful.key({ modkey }, "space",  awful.client.floating.toggle                     ,
-    {description = "toggle floating", group = "client"}),
-    awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
-    {description = "move to master", group = "client"}),
-    awful.key({ modkey }, "o",      function (c) c:move_to_screen()               end,
-    {description = "move to screen", group = "client"}),
-    awful.key({ modkey }, "t",      function (c) c.ontop = not c.ontop            end,
-    {description = "toggle keep on top", group = "client"}),
-    awful.key({ modkey }, "n",
-    function (c)
-        -- The client currently has the input focus, so it cannot be
-        -- minimized, since minimized clients can't have the focus.
-        c.minimized = true
-    end ,
-    {description = "minimize", group = "client"}),
-    awful.key({ modkey,           }, "m",
-    function (c)
-        c.maximized = not c.maximized
-        c:raise()
-    end ,
-    {description = "(un)maximize", group = "client"}),
-    awful.key({ modkey, "Control" }, "m",
-    function (c)
-        c.maximized_vertical = not c.maximized_vertical
-        c:raise()
-    end ,
-    {description = "(un)maximize vertically", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "m",
-    function (c)
-        c.maximized_horizontal = not c.maximized_horizontal
-        c:raise()
-    end ,
-    {description = "(un)maximize horizontally", group = "client"})
+    awful.key({ modkey }, "f",  awful.client.floating.toggle                     ,
+    {description = "toggle floating", group = "client"})
 )
 
 -- Bind all key numbers to tags.
